137dd2e5cdd06408479f5e486096806f2ca68ef5
ducciorocchini <ducciorocchini@gmail.com>
2020-11-09 10:45:04 +0100

    Update R_code_plot_ecol_var.r

--- R_code_plot_ecol_var.r
+++ R_code_plot_ecol_var.r
@@ -47,6 +47,56 @@ pairs(meuse)
 # pairing only the elements part of the dataset: how to do that?
 # only with cadmium, copper, lead, zinc...
 
+##############################
+
+# Lecture #2 on ecological variables
+
+# Exercise
+# Recall the package sp, recall the dataset meuse
+
+library(sp)
+data(meuse)
+
+pairs(meuse)
+
+head(meuse)
+
+# cadmium copper lead zinc
+# pairs with soil variables
+# from column 3 to column 6
+pairs(meuse[,3:6]) # how to do quadratic parathenses: AltGr + è
+
+# let's use the names of the columns:
+pairs(~ cadmium + copper + lead + zinc, data=meuse) 
+# tilde is going to be done by AltGr + ^
+# in windows: Alt + 0126 or Alt + 126
+# In Mac: alt + n on mac
+
+# let's prettify the graph
+# Exercise: just use cadmium, lead and zince
+pairs(~ cadmium + lead + zinc, data=meuse)
+
+# Exercise: change the color
+pairs(~ cadmium + copper + lead + zinc, data=meuse, col="red") 
+
+# for the future change colours of single panels by the par() function 
+
+# Exercise: change the symbol to filled triangles: pch
+pairs(~ cadmium + copper + lead + zinc, data=meuse, col="red", pch=17) 
+
+# nice page on pairs:
+# https://statisticsglobe.com/r-pairs-plot-example/
+
+# Exercise: increase the size of the trinagles
+pairs(~ cadmium + copper + lead + zinc, data=meuse, col="red", pch=17, cex=3) 
+
+
+
+
+
+
+
+
 
 
 
