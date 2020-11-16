d7049aa1cc68f439a5507ff3076e91825994c860
ducciorocchini <ducciorocchini@gmail.com>
2020-11-16 10:58:10 +0100

    Update R_spatial.r

--- R_spatial.r
+++ R_spatial.r
@@ -30,6 +30,70 @@ bubble(meuse, "zinc")
 bubble(meuse, "lead")
 bubble(meuse, "lead", col="red")
 
+###### Installing the ggplot2 library / package
+
+install.packages("ggplot2")
+
+library(ggplot2)
+
+# ecological dataframe
+# biofuels
+biofuels <- c(120, 200, 350, 570, 750) # array of values: c
+biofuels
+# oxydative enzimes
+oxydative <- c(1200, 1300, 21000, 34000, 50000) 
+
+# dataframe
+d <- data.frame(biofuels, oxydative)
+d
+
+# Is it the same to do data.frame(biofuels,oxydative)? yes!
+ggplot(d, aes(x = biofuels, y = oxydative)) + geom_point()
+
+ggplot(d, aes(x = biofuels, y = oxydative)) + geom_point(size = 5, col = "red")
+
+# lines
+ggplot(d, aes(x = biofuels, y = oxydative)) + geom_line() 
+
+# points + lines
+ggplot(d, aes(x = biofuels, y = oxydative)) + geom_point(size = 5, col = "red")  +  geom_line() 
+
+# polygons
+ggplot(d, aes(x = biofuels, y = oxydative)) + geom_polygon()
+
+############## IMPORT DATA FROM AN EXTERNAL SOURCE
+
+# setwd("path/lab")
+
+# setwd for Windows
+setwd("C:/lab/")
+
+# setwd Mac
+setwd("/Users/yourname/lab/")
+
+# Linux
+setwd("~/lab/")
+covid <- read.table("covid_agg.csv", header=TRUE)
+covid
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
