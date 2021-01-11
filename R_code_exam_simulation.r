6569af68d4ffc1f70a404ffdc2869df4cb1d72b1
ducciorocchini <ducciorocchini@gmail.com>
2021-01-11 11:05:14 +0100

    Update R_code_exam_simulation.r

--- R_code_exam_simulation.r
+++ R_code_exam_simulation.r
@@ -6,22 +6,24 @@ https://land.copernicus.vgt.vito.be/PDF/portal/Application.html
 
 # install.packages("ncdf4")
 
-  library(ncdf4)
-  library(raster)
+library(ncdf4)
+library(raster)
 
-  setwd("~/lab/")
 
-  # importing images
-  tjan <- raster("c_gls_LST10-DC_202101010000_GLOBE_GEO_V1.2.1.nc")
-  plot(tjan)
+setwd("~/lab/") # Linux
+# setwd("C:/lab/")  # windows
+# setwd("/Users/name/lab/") # mac
+# importing images
+tjan <- raster("c_gls_LST10-DC_202101010000_GLOBE_GEO_V1.2.1.nc")
+plot(tjan)
 
-  # Exercise: change the colorramppalette
-  cltjan <- colorRampPalette(c('blue','yellow','red'))(100) # 
-  plot(tjan, col=cltjan)
+# Exercise: change the colorramppalette
+cltjan <- colorRampPalette(c('blue','yellow','red'))(100) # 
+plot(tjan, col=cltjan)
 
-  toct <- raster("c_gls_LST10-DC_202010010000_GLOBE_GEO_V1.2.1.nc")
-  plot(toct, col=cltjan)
+toct <- raster("c_gls_LST10-DC_202010010000_GLOBE_GEO_V1.2.1.nc")
+plot(toct, col=cltjan)
 
-  dift <- tjan - toct
-  cldif <- colorRampPalette(c('blue','white','red'))(100) # 
-  plot(dift, col=cldif)
+dift <- tjan - toct
+cldif <- colorRampPalette(c('blue','white','red'))(100) # 
+plot(dift, col=cldif)
